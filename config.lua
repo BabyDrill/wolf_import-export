847a68e36f3ce037015ca4f27e04275193e59faa
BabyDrill <96079788+BabyDrill@users.noreply.github.com>
2022-02-20 22:49:17 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,23 @@
+--Import Export by BabyDrill#7768-- Contattatemi se vi server altro :)
+Config = {}
+
+Config.Locale = 'it'
+
+Config.ItemOggetti = { --Qui potrete mettere tutti gli oggetti che vorreste vendere all'import con affianco il prezzo
+	copper = 10,
+	iron = 30,
+	gold = 50,
+	diamond = 200,
+	packaged_plank = 20,
+	fish = 20,
+	packaged_chicken = 20,
+	clothe = 15,
+	essence = 20,
+}
+
+
+Config.SoldiSporchi = false  --Impostato su false l'import export non paghera con soldi sporchi ma ben si con soldi puliti
+
+Config.ZonaBlip = {
+	Allrounddealer = {coords = vector3(1226.49, -3234.11, 6.03), name = 'Import Export', color = 6, sprite = 478, radius = 25.0} --Blip modificabile a seconda delle cordinate
+}
